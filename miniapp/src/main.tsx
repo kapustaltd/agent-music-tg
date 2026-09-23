ac6567f2f36e8ffab1f1dcfaf52de1931f389bd0
xdshkaaa <83063989+xdshkaaa@users.noreply.github.com>
2026-09-23 11:03:51 +0700

    fix: tighten artist detail layout

--- miniapp/src/main.tsx
+++ miniapp/src/main.tsx
@@ -9,6 +9,7 @@ import "./styles/generated-playlist.css";
 import "./styles/anti-slop.css";
 import "./styles/generation-loading.css";
 import "./styles/clarify-flow.css";
+import "./styles/artist-screen.css";
 
 const storedScheme = typeof localStorage !== "undefined" ? localStorage.getItem("miniapp-scheme") : null;
 document.documentElement.setAttribute(
